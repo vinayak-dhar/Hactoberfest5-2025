dff15057c5618dee514f68e97f3ff697e639137b
maheswari2418 <maheswari1187@gmail.com>
2025-10-16 12:29:37 +0530

    Added React + Tailwind Word-to-PDF project

---
+++ WordToPdf/WordToPdf-main/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+      <div>
+        <hr className="border-black" />
+        <h1 className="text-center py-3 text-sm">
+          Copyright ⓒ 2024 - All right reserved
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
